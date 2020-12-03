e0f0a68ffc0a483b61b8f2b3e5674327b33308f6
Federico <federico.taschin@studenti.unipd.it>
2020-12-03 13:41:04 +0100

    Add utils.py with Counter class and cuda func

---
+++ utils.py
@@ -0,0 +1,23 @@
+import torch
+from torch import multiprocessing
+
+
+class Counter:
+    """Shared counter implementation, from https://github.com/Kaixhin/ACER/blob/master/utils.py
+    """
+
+    def __init__(self):
+        self.val = multiprocessing.Value('i', 0)
+        self.lock = multiprocessing.Lock()
+
+    def increment(self, v=1):
+        with self.lock:
+            self.val.value += v
+
+    def value(self):
+        with self.lock:
+            return self.val.value
+
+
+def cuda_if(tensor):
+    return tensor.cuda() if torch.cuda.is_available() else tensor
